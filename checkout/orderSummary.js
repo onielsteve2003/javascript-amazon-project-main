49789f0fead80312aca516d2ae3d7850f40bd1d1
kachy <kachigirl54@gmail.com>
2026-01-29 16:18:40 +0100

    save data and generate html for payment summary

--- checkout/orderSummary.js
+++ checkout/orderSummary.js
@@ -6,10 +6,10 @@ import {
   updateDeliveryOption
 } from "../scripts/cart.js";
 
-import { products } from "../data/products.js";
+import { products, getProduct } from "../data/products.js";
 import { formatCurrency } from "../utilities/money.js";
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
-import { deliveryOptions } from "../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOption } from "../data/deliveryOptions.js";
 
 
 const today = dayjs();
@@ -32,23 +32,11 @@ cart.forEach((cartItem)=>{
     const productId = cartItem.productId
 
 
-let matchingProduct ;
-
-products.forEach((product) => {
-    if(product.id === productId){
-        matchingProduct = product
-    }
-})
+const matchingProduct = getProduct(productId);
 
 const deliveryOptionId = cartItem.deliveryOptionsId;
 
-let deliveryOption;
-
-deliveryOptions.forEach((option)=>{
-    if (option.id === deliveryOptionId){
-        deliveryOption = option;
-    }
-});
+const deliveryOption = getDeliveryOption(deliveryOptionId);
 
    const today = dayjs();
         const deliveryDate = today.add(
